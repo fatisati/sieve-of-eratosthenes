a3f05033a26e6abb35eecf4532024a9b826e7a6b
fateme <fatisati@gmail.com>
2017-11-03 09:47:29 -0400

    multithread and working:))

--- linux_thread2.c
+++ linux_thread2.c
@@ -3,14 +3,18 @@
 #include <pthread.h>
  
 #define NUM_THREADS 100
-int size;
-int *arr;
-pthread_t *thr;
+
 /* create thread argument struct for thr_func() */
 typedef struct _thread_data_t {
   int tid;
   double stuff;
 } thread_data_t;
+
+int size;
+int *arr;
+int *check;
+pthread_t *thr;
+thread_data_t *thr_data;
  
 /* thread function */
 void *thr_func(void *arg) {
@@ -20,7 +24,7 @@ void *thr_func(void *arg) {
 
   long j;
   for(j= i*i; j<size; j= j+i){
-  	arr[j]++;
+  	arr[j] = 0;
   }
  
   pthread_exit(NULL);
@@ -29,22 +33,26 @@ void *thr_func(void *arg) {
 int main(int argc, char **argv) {
 
   int i, rc;
-  size = 100;
+  size = 1000000;
   arr = (int *)malloc(sizeof(int) * size);
-  thr = (int *)malloc(sizeof(pthread_t) * size);
+  check = (int *)malloc(sizeof(int) * size);
+
+  thr = (pthread_t *)malloc(sizeof(pthread_t) * size);
+  thr_data = (thread_data_t *)malloc(sizeof(thread_data_t) * size);
+
   for(i=0; i<size; i++){
-  	arr[i] = 0;
+  	arr[i] = 1;
+	check[i]=0;
   }
 
   //pthread_t thr[size];
   /* create a thread_data_t argument array */
-  thread_data_t thr_data[size];
+  //thread_data_t thr_data[size];
  
   /* create threads */
   int flag= 1;
   for (i = 2; i < size; i++) {
 
-    arr[i]++;
     if(arr[i]==1){
 	
 	if(flag){
@@ -54,26 +62,31 @@ int main(int argc, char **argv) {
 		}
 		else{
     			thr_data[i].tid = i;
+			check[i]=1;
     			if ((rc = pthread_create(&thr[i], NULL, thr_func, &thr_data[i]))) {
       				fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
       				return EXIT_FAILURE;
     			}
-			pthread_join(thr[i], NULL);
 		}
 	}
 			
     }
   }
   /* block until all threads complete */
-  /*for (i = 0; i < size; ++i) {
-	pthread_join(thr[i], NULL);
-  }*/
-
   for (i = 0; i < size; ++i) {
-	if(arr[i]==1){
-		printf("%d ",i);
+	if(check[i]){
+	pthread_join(thr[i], NULL);
 	}
   }
+  int count =0;
+
+  for (i = 2; i < size; ++i) {
+	if(arr[i]==1){
+		printf("%d ",i);
+		count++;
+	}
+  }
+  printf("%d", count);
  
   return EXIT_SUCCESS;
 }
